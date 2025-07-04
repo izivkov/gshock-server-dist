d02df7fb606f5b29b73483744d4c4e9204bca657
Just Me <izivkov@gmail.com>
2025-07-04 11:46:32 -0400

    Automated update from make_dist.sh on 2025-07-04 11:46:32

--- setup.sh
+++ setup.sh
@@ -11,6 +11,11 @@ sudo apt install -y zip unzip
 sudo apt install -y libfreetype6-dev
 sudo apt install -y libjpeg-dev zlib1g-dev libopenjp2-7-dev libtiff5-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk
 
+# Setup virtual environmsnent
+sudo apt install python3-venv
+python3 -m venv venv
+source venv/bin/activate
+
 # Install dependencies
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
@@ -25,7 +30,8 @@ Description=G-Shock Time Server
 After=network.target
 
 [Service]
-ExecStart=python3 /home/pi/gshock-server-dist/gshock_server.py --multi-watch
+ExecStart=/home/pi/gshock-server-dist/venv/bin/python /home/pi/gshock-server-dist/gshock_server.py --multi-watch
+# ExecStart=python3 /home/pi/gshock-server-dist/gshock_server.py --multi-watch
 WorkingDirectory=/home/pi/gshock-server-dist
 Environment=PYTHONUNBUFFERED=1
 Restart=on-failure
