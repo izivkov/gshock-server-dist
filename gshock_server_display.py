946e4e49612cc0559aa5f2bd69e83acdb09550f1
Just Me <izivkov@gmail.com>
2025-07-16 22:08:26 -0400

    Automated update from make_dist.sh on 2025-07-16 22:08:26

--- gshock_server_display.py
+++ gshock_server_display.py
@@ -21,7 +21,7 @@ from peristent_store import PersistentMap
 __author__ = "Ivo Zivkov"
 __copyright__ = "Ivo Zivkov"
 __license__ = "MIT"
-__tag__ = "v1.0.32"
+__tag__ = "v1.0.34"
 
 # This script is used to set the time on a G-Shock watch and display information on a connected display.
 
@@ -146,7 +146,7 @@ async def run_time_server():
             run_once_key(
                 "show_welcome_screen",
                 oled.show_welcome_screen,
-                "Waiting\nfor connection...32.1",
+                "Waiting\nfor connection...34.1",
                 watch_name=store.get("watch_name", None),
                 last_sync=store.get("last_connected", None),
             )
@@ -183,7 +183,7 @@ async def run_time_server():
                 await safe_show_display(api)
             else:
                 oled.show_welcome_screen(
-                    "Waiting\nfor connection...32.2",
+                    "Waiting\nfor connection...34.2",
                     watch_name=store.get("watch_name", None),
                     last_sync=store.get("last_connected", None),
                 )
@@ -199,7 +199,7 @@ async def run_time_server():
             logger.error(f"Got error: {e}")
 
             oled.show_welcome_screen(
-                "Waiting\nfor connection...32.3",
+                "Waiting\nfor connection...34.3",
                 watch_name=store.get("watch_name", None),
                 last_sync=store.get("last_connected", None),
             )
