f36c1a88827612b1a589ab868c02ea7e1d9ee04a
Ivo Zivkov <izivkov@gmail.com>
2025-11-17 21:43:31 -0500

    Automated update from make_dist.sh on 2025-11-17 21:43:31

--- setup-boot.sh
+++ setup-boot.sh
@@ -33,7 +33,7 @@ EOL
 chmod +x "$BOOT_SCRIPT"
 
 # Create and enable BOOT service
-SERVICE_FILE="$USER_SYSTEMD_DIR/user-boot-script.service"
+BOOT_SERVICE_FILE="$USER_SYSTEMD_DIR/user-boot-script.service"
 sudo tee "$BOOT_SERVICE_FILE" > /dev/null <<EOL
 [Unit]
 Description=Run Pi user’s boot script
